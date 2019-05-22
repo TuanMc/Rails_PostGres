1148685351765eee8d62334e65e74620791d7634
mctuan <mctuan@mmsofts.com>
2019-05-22 20:40:53 +0700

    Add password for edit and create user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_16_122557) do
+ActiveRecord::Schema.define(version: 2019_05_21_174131) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2019_05_16_122557) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password"
   end
 
 end
