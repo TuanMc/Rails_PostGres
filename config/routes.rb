d77fe742ede53d12cf590bf39b5cb05c452a26a2
mctuan <mctuan@mmsofts.com>
2019-05-30 00:50:24 +0700

    Add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, :controllers => {
+    :registrations => "register"
+  }
   get 'static_pages/about'
   get 'static_pages/home'
   get 'static_pages/help'
@@ -7,4 +10,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ 
 end
